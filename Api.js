fa63249eef725ac841126ae021f7cfb4b596786e
Jurgen Treep <jurgen.treep@gmail.com>
2018-05-19 16:14:17 +0200

    fix: a big list of mappers resulted in exceeding the rate limit
    Resolved this by doing the calls to retrieve the beatmap ids in serial.

--- Api.js
+++ Api.js
@@ -1,6 +1,10 @@
 const https = require('https');
 const Config = require('./Config');
 
+/**
+ * Don't go over 1200 requests per second you'll get an captcha.
+ * Luckily this is removed after a while but just keep it in mind.
+ */
 module.exports = class Api {
     constructor() {
         this.apiKey = process.env.API_KEY;
@@ -10,17 +14,36 @@ module.exports = class Api {
         return new Promise((resolve, reject) => {
             const config = new Config();
 
-            config.getMappers().then(mappers => {
-                const promises = mappers.map(mapper => {
-                    return this.getBeatmapIdsFromMapper(mapper);
-                });
+            config.getMappers()
+                .then(mappers => {
+                    /**
+                     * I copied the following piece of code and still don't fully understand
+                     * what's going on.
+                     *
+                     * It creates functions which return promises. Then it reduces the array
+                     * of functions and executes each promise. The result of each promise
+                     * get's concatonated onto the array.
+                     */
+                    const promiseSerial = funcs =>
+                        funcs.reduce((promise, func) =>
+                            promise.then(result => func().then(Array.prototype.concat.bind(result))),
+                            Promise.resolve([]))
 
-                Promise.all(promises).then(results => {
-                    console.info('Succesfully retrieved beatmap id\'s for the list of mappers');
-                    resolve([].concat(...results));
-                })
-                    .catch(console.error);
-            });
+                    // convert each url to a function that returns a promise
+                    const funcs = mappers.map(mapper => () => this.getBeatmapIdsFromMapper(mapper))
+
+                    console.time('Time to collect all beatmap ids')
+
+                    // execute Promises in serial
+                    promiseSerial(funcs)
+                        .then(results => {
+                            console.info(`Retrieved a total of ${results.length} beatmap ids`)
+                            console.timeEnd('Time to collect all beatmap ids')
+
+                            resolve(results)
+                        })
+                        .catch(reject)
+                });
         });
     }
 
@@ -36,23 +59,21 @@ module.exports = class Api {
             let rawData = '';
 
             https.request(options, res => {
-                if (res.statusCode === 200) {
-                    res.setEncoding('utf8');
+                res.setEncoding('utf8');
 
-                    res.on('data', chunk => rawData += chunk);
+                res.on('data', chunk => rawData += chunk);
 
-                    res.on('end', () => {
-                        console.log(rawData);
+                res.on('end', () => {
+                    if (res.statusCode === 200) {
                         resolve(
                             Array.from(new Set(
                                 JSON.parse(rawData)
-                                    .map(metadata => metadata.beatmapset_id)
-                            ))
+                                    .map(metadata => metadata.beatmapset_id)))
                         )
-                    });
-                } else {
-                    reject(options.path);
-                }
+                    } else {
+                        reject(`error while trying to get beatmap ids for ${options.path}`);
+                    }
+                });
             })
                 .on('error', reject)
                 .end();
